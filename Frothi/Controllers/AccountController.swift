ed63b19feddda637d6ff12a3e29cb510b27f1bca
Nicholas Wargnier <nwargnier@gmail.com>
2014-09-23 23:54:49 -0700

    refactoring login storyboard

--- Frothi/Controllers/AccountController.swift
+++ Frothi/Controllers/AccountController.swift
@@ -15,7 +15,6 @@ class AccountController : UIViewController {
     super.viewDidLoad()
     
     self.navigationController?.navigationBar
-    
 
     menuButton.target = self.revealViewController()
     menuButton.action = Selector("revealToggle:")
@@ -25,30 +24,6 @@ class AccountController : UIViewController {
     let avenirNext:UIFont = UIFont(name: "Avenir Next", size: 20)
     let titleDict:NSDictionary = [NSFontAttributeName: avenirNext,NSForegroundColorAttributeName: UIColor.whiteColor()]
     navigationController?.navigationBar.titleTextAttributes = titleDict
-    
-    
-//    navigationController?.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "CaviarDreams", size: 20)]
-//    NSShadow* shadow = [NSShadow new];
-//    shadow.shadowOffset = CGSizeMake(0.0f, 1.0f);
-//    shadow.shadowColor = [UIColor redColor];
-//    [[UINavigationBar appearance] setTitleTextAttributes: @{
-//      NSForegroundColorAttributeName: [UIColor greenColor],
-//      NSFontAttributeName: [UIFont fontWithName:@"Helvetica" size:20.0f],
-//      NSShadowAttributeName: shadow
-//    }];
-    
-    
-
-//    self.title = NSLocalizedString(@"Front View", nil);
-//    
-//    SWRevealViewController *revealController = [self revealViewController];
-//    
-//    [self.navigationController.navigationBar addGestureRecognizer:revealController.panGestureRecognizer];
-//    
-//    UIBarButtonItem *revealButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"reveal-icon.png"]
-//    style:UIBarButtonItemStyleBordered target:revealController action:@selector(revealToggle:)];
-//    
-//    self.navigationItem.leftBarButtonItem = revealButtonItem;
   }
   
 }
